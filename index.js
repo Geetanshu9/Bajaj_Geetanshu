46267bde6b62cf03c60728b0aff04c7290d8f9cc
Geetanshu9 <101512109+Geetanshu9@users.noreply.github.com>
2024-03-18 13:43:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 app.use(express.json());
+app.use(cors());
 const port = 8000;
 app.get("/", (req, res) => {
   res.send("Bajaj Api");
